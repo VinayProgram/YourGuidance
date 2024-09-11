60fa54c8de544ff4b232dde0d10bb4272f722637
VinayProgram <Vinaytandale@gmail.com>
2024-09-11 10:26:53 +0530

    login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
-import { signIn } from '@/auth'
+
 export async function GET() {
-    await signIn('credentials', { 'email', 'password' })
+
     return NextResponse.json('hello')
     
 }
