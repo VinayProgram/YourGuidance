679c51412a840701a37e01966c0ded6f8b688010
VinayProgram <Vinaytandale@gmail.com>
2024-09-12 10:03:14 +0530

    login

---
+++ src/app/SignIn/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React from "react";
+import LoginForm from "./components/login";
+import "./login.css";
+import { Button, Card } from "antd";
+import { signInWithPopup, onAuthStateChanged } from 'firebase/auth';
+import { auth, provider } from "@/config";
+const Login = () => {
+  React.useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+       console.log(user)
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error('Google Sign-In Error:', error);
+    }
+  };
+
+
+  return (
+    <main className="main">
+      <Card title="login">
+        <LoginForm />
+        <Button onClick={handleGoogleSignIn}>Sign In with Google</Button>
+      </Card>
+      
+    </main>
+  );
+};
+
+export default Login;
