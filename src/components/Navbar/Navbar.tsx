c3ae81203f72abe6524de4512090d17225ec4faa
VinayProgram <vinaytandale@gmail.com>
2024-10-13 00:12:47 +0530

    personal posts and login image

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useCommonStore } from "@/store/CommonStore";
-import { Button, Layout, theme } from "antd";
+import { Button, Flex, Layout } from "antd";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
@@ -10,17 +10,17 @@ const { Header } = Layout;
 import React from "react";
 
 const Navbar = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
   const { sidebarActive, setSidebar } = useCommonStore();
   return (
-    <Header style={{ padding: 0, background: colorBgContainer }}>
+    <Header style={{ padding: 0, background: "#001529",color:"white" }}>
+  <Flex justify="space-between" align="center" gap={2} >
       <Button
         type="text"
-        icon={sidebarActive ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        icon={sidebarActive ? <MenuUnfoldOutlined color="white" style={{color:"white"}}/> : <MenuFoldOutlined  style={{color:"white"}} color="white"/>}
         onClick={() => setSidebar(!sidebarActive)}
       />
+          <h1 style={{color:'white'}}><b>Your</b>-<i style={{color:'yellow'}}>Guidance</i></h1>
+          </Flex>
     </Header>
   );
 };
